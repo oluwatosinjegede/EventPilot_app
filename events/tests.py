766e156da4db78a3e65e484cc4a75a5a85dac774
oluwatosinjegede <jegedeoluwatosin10@gmail.com>
2026-05-13 16:25:27 +0100

    Refactor registration button links

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeRegistrationLinkTests(TestCase):
+    def test_home_links_registration_buttons_to_correct_flows(self):
+        response = self.client.get(reverse('home'))
+
+        self.assertContains(response, 'Event planner registration')
+        self.assertContains(response, 'href="/accounts/register/"')
+        self.assertContains(response, 'Vendor registration')
+        self.assertContains(response, 'href="/vendors/register/"')
+        self.assertContains(response, 'Join event')
